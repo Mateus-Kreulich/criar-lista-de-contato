b011843676f96eb7f41372b07a276c094fcb6b04
Mateus-Kreulich <mateuskreulich@hotmail.com>
2024-07-11 22:55:30 -0300

    --amend

--- contatos.py
+++ contatos.py
@@ -66,3 +66,4 @@ def menu():
 
 if __name__ == "__main__":
     menu()
+ 
--- executar_contatos.bat
+++ executar_contatos.bat
@@ -1,8 +1,9 @@
 @echo off
-rem Define o caminho para o script Python
-set SCRIPT_PYTHON="C:\Users\User\3D Objects\papyros\contatos.py"
+rem Define o caminho relativo para o script Python
+set SCRIPT_PYTHON=contatos.py
 
 rem Executa o script Python
 python %SCRIPT_PYTHON%
 
 pause
+ 
